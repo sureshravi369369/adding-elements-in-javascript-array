4b9093578aecd52babf629dc1fc0e4577cf9e394
Suresh <114983422+sureshravi369369@users.noreply.github.com>
2022-11-14 17:26:38 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,18 @@
+// add elements to an javascript array:
+
+const items=["apple","orange","grapes"]
+
+// push
+
+items.push("lemon","strawbarries")
+
+// unshift
+
+items.unshift("gauva")
+
+// slice
+
+items.splice(2,0,"watermwlons","bananas")
+
+
+console.log(items);
